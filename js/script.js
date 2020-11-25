05cd5fdf5e2ea5b8d9a7daec950e7396b99efe90
MAlik <malikimenov@gmail.com>
2020-11-26 00:19:22 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use sctrict";
+// let number = 4.6;
+
+// console.log(4/0);
+// console.log('str' * 9);
+// console.log(something);
+
+// const obj = {
+//     name: 'John',
+//     age: 26,
+//     isMarried: false
+// }
+// console.log(obj);
+
+// let arr = ['plum.pmg', 'orange.jpg', 8, 'apple.txt', {}, []];
+// console.log(arr);
+
+
+// let result = +prompt('Вы есть 18?', '18');
+// console.log(typeof(result));
+
+// const answers = [];
+// answers[0] = prompt("Как вас зовут?", '');
+// answers[1] = prompt('Какая у вас фамилия', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+// document.write(answers);
+
+const category = 'tous';
+console.log(`https//some.rt.com/${category}/5`);
+const user = 'ivan';
+alert(`Hello ${user}`);
